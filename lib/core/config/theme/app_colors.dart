59b1b86996aed800b3f0d6f2fb0267bec33a4f2d
angelstchavez <angelstchavez@gmail.com>
2025-08-19 15:22:23 -0500

    feat: config theme and fonts

---
+++ lib/core/config/theme/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF1B5E20);
+  static const secondary = Color(0xFF388E3C);
+  static const background = Color(0xFFE8F5E9);
+  static const error = Color(0xFFB71C1C);
+  static const text = Color(0xFF212121);
+  static const accent = Color(0xFF66BB6A);
+
+  static const darkPrimary = Color(0xFF2E7D32);
+  static const darkSecondary = Color(0xFF43A047);
+  static const darkBackground = Color(0xFF121212);
+  static const darkText = Color(0xFFFFFFFF);
+  static const darkAccent = Color(0xFF81C784);
+}
---
+++ lib/core/config/theme/app_theme.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/core/config/theme/app_colors.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.background,
+    brightness: Brightness.light,
+    fontFamily: 'Satoshi',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: AppColors.text,
+        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      ),
+    ),
+  );
+
+  static final darkTheme = ThemeData(
+    primaryColor: AppColors.darkPrimary,
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    brightness: Brightness.dark,
+    fontFamily: 'Satoshi',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.darkPrimary,
+        textStyle: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: AppColors.darkText,
+        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      ),
+    ),
+  );
+}
