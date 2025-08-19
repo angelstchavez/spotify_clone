710dfb75f0edcb5d23daca7a0292991cf0e6600b
angelstchavez <angelstchavez@gmail.com>
2025-08-19 15:49:48 -0500

    feat: add initial splash page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spotify_clone/core/config/theme/app_theme.dart';
+import 'package:spotify_clone/presentation/pages/splash/splash_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: AppTheme.lightTheme, home: Container());
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
   }
 }
